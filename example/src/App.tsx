331e80fd8abe0862e5d3d78e7432f5f35f06efd4
hotchemi <hot.chemistry21@gmail.com>
2018-01-08 23:22:22 +0900

    Tweak sample code.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import TreasureData from 'react-native-td';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import TreasureData from "react-native-td";
 
 interface Props {}
 
 interface State {}
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 const styles: any = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
 
 export default class App extends Component<Props, State> {
@@ -39,17 +39,36 @@ export default class App extends Component<Props, State> {
 
   componentDidMount() {
     TreasureData.addEventWithCallback(
-      { event: 'event_name' },
-      'databse_name',
-      'record_name',
+      { event: "event_name" },
+      "databse_name",
+      "record_name",
       () => {
-        console.log('onSuccess is called.');
+        console.log("onSuccess is called.");
       },
       (error, message) => {
         console.log(`Error: ${error}`);
         console.log(`Message: ${message}`);
       }
     );
+  }
+
+  componentWillUnmount() {
+    TreasureData.isFirstRun()
+      .then(() => {
+        TreasureData.clearFirstRun();
+        TreasureData.uploadEventsWithCallback(
+          () => {
+            console.log("onSuccess is called.");
+          },
+          (error, message) => {
+            console.log(`Error: ${error}`);
+            console.log(`Message: ${message}`);
+          }
+        );
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
